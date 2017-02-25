b698c39aa4e4f3051f32ef6b8594e899819c8751
aaggarw99 <aaggarw99@gmail.com>
2017-02-25 01:08:22 -0600

    graph essentials

--- BitBox/BitBox/LiveController.swift
+++ BitBox/BitBox/LiveController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class LiveController: UIViewController {
 
+    @IBOutlet weak var graphView: GraphView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
 
 
 }
