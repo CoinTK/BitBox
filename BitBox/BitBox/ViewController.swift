06faadb4066dc4af90c86028b52d2810740e7206
aaggarw99 <aaggarw99@gmail.com>
2017-02-24 23:04:24 -0600

    swift framework

---
+++ BitBox/BitBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitBox
+//
+//  Created by Ashwin Aggarwal on 2/24/17.
+//  Copyright © 2017 Ashwin Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
